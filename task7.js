83da1bac6c57f198fa8f707ea900427215f8df19
Chiqa <omenkachinyere@gmail.com>
2026-03-10 13:15:51 +0100

    task7.js

---
+++ task7.js
@@ -0,0 +1,13 @@
+// Initialize side lengths
+let side1 = 5;
+let side2 = 5;
+let side3 = 5;
+
+// Check triangle type
+if (side1 === side2 && side2 === side3) {
+    console.log("Equilateral triangle");
+} else if (side1 === side2 || side1 === side3 || side2 === side3) {
+    console.log("Isosceles triangle");
+} else {
+    console.log("Scalene triangle");
+}
