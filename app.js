d9cf1344888bdf59517c9fbda7439910eaeccd9e
Chiqa <omenkachinyere@gmail.com>
2026-03-10 13:21:01 +0100

    task7.js

--- app.js
+++ app.js
@@ -13,9 +13,27 @@ const lastName = 'Friend';
 const fullName = firstName + ' ' + lastName;
 console.log(fullName);
 
-const age = 16;
-if (age >= 18) {
-    console.log('You are an adult and eligible to vote.');
-} else {
-    console.log('You are not an adult and not eligible to vote.');
+//const age = 16;
+//if (age >= 18) {
+   // console.log('You are an adult and eligible to vote.');
+//} else {
+//    console.log('You are not an adult and not eligible to vote.');
+//}
+
+// const age = 100;
+// if (age>=25) {
+//     console.log('You are eligible to rent a car.');
+// } else if (age>=57) {
+//     console.log('You are not eligible to rent a car.')
+// }  else {
+//     console.log('You are too young to rent a car.');
+// }
+
+const age = 59;
+if (age>=25 && age<=57) {
+    console.log('You are eligible to rent a car.');
+} else if (age>=57) {
+    console.log('You are not eligible to rent a car.')
+}  else {
+    console.log('You are too young to rent a car.');
 }
